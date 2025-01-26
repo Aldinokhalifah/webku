249635135039ed35cb5a9b5c30c2a5efcc88d1a9
Aldinokhalifah <khalifahaldino@gmail.com>
2025-01-26 19:39:09 +0700

    Membuat Middleware

---
+++ Next JS/app-router/src/middleware.ts
@@ -0,0 +1,14 @@
+// import { request } from "http";
+import { NextResponse } from "next/server";
+import{ NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+    const isLogin = false;
+    if(!isLogin) {
+        return NextResponse.redirect(new URL("/login", request.url));
+    }
+}
+
+export const config = {
+    matcher: ["/dashboard/:path*", "/about/:path*"],
+};
