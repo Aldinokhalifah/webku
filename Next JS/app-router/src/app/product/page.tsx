d8555bd0f1e548aaeea88fbf6dadc810bd921329
Aldinokhalifah <khalifahaldino@gmail.com>
2025-01-30 06:36:06 +0700

    Membuat Intercepting Route

--- Next JS/app-router/src/app/product/page.tsx
+++ Next JS/app-router/src/app/product/page.tsx
@@ -1,49 +1,34 @@
-import { cache } from "react";
+import {getData} from "@/services/products";
+import Link from "next/link";
 
 type ProductPageProps = {params: {slug: string }}
-
-async function getData() {
-    // const res = await fetch('https://fakestoreapi.com/products');
-    const res = await fetch('http://localhost:3000/api/product', {
-    cache: 'no-store',
-    next: {
-        // revalidate: 30
-        tags: ['products']
-    }
-    });
-
-    if(!res.ok) {
-        throw new Error('Failed to fetch data');
-    }
-    return res.json();
-}
 
 
 export default async function DetailPage(props: ProductPageProps) {
     const {params} = props;
-    const products = await getData();
+    const products = await getData('http://localhost:3000/api/product');
     console.log(products);
     return (
         <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 place-items-center">
             {/* <h1>{params.slug ? "Detail Product Page" : "Product Page"} </h1> */}
             {products.data.length > 0 && products.data.map((product : any) => (
-            <div key={product.id} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-2">
-                <a href="#">
+            <Link href={`/product/detail/${product.id}`} key={product.id} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-2">
+            
                     <img 
                     className="p-8 rounded-t-lg object-cover h-96 w-full" 
                     src={product.image} 
                     alt="product image" />
-                </a>
+            
                 <div className="px-5 pb-5">
-                    <a href="#">
+                
                         <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white truncate">{product.title}</h5>
-                    </a>
+                
                     <div className="flex items-center justify-between mt-3">
                         <span className="text-3xl font-bold text-gray-900 dark:text-white">$ {product.price}</span>
-                        <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                        <button type="button"  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</button>
                     </div>
                 </div>
-            </div>
+            </Link>
             ))}
             {params.slug && (
                 <>
